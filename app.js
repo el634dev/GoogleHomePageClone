28cae6d431add4ba39c75bfc3a9e8dc486aecbba
el634dev <84924260+el634dev@users.noreply.github.com>
2021-09-30 11:46:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const searchInput = document.querySelector("#search.input");
+const searchInput = document.querySelector("#search-input");
 
-search.addEventListener("keydown", function (event) {
-  if (event.code === "Enter") search();
+searchInput.addEventListener("keydown", function(event) {
+    if(event.code === "Enter") {
+        search();
+    }
 });
 
 function search() {
-  const input = searchInput.value;
+    const input = searchInput.value;
 
-  window.location.href =
-    "https://www.google.com/search?q" +
-    input +
-    "&source=hp&ei=8ThOYc3HIrqi5NoP_8KuoAg&iflsig=ALs-wAMAAAAAYU5HAYo76bN2NeMM_MNneksr7qH3wGiw&oq" +
-    input +
-    "&gs_lcp=Cgdnd3Mtd2l6EAMyEQguEIAEELEDEMcBENEDEJMCMggILhCABBCxAzIOCC4QgAQQsQMQxwEQ0QMyBQgAEIAEMggIABCABBCxAzIFCAAQgAQyCAgAEIAEELEDMgUIABCABDIFCAAQgAQyCAgAEIAEELEDOhEILhCABBCxAxDHARCjAhCTAjoLCAAQgAQQsQMQgwE6CAguELEDEIMBOg4ILhCABBCxAxDHARCjAjoLCC4QgAQQxwEQrwE6EQguEIAEELEDEMcBEK8BEJMCOgsILhCxAxDHARCjAjoFCC4QgAQ6DgguEIAEELEDEMcBEK8BUM8GWMkeYIcjaABwAHgAgAG5AYgBhQSSAQM0LjGYAQCgAQE&sclient=gws-wiz&ved=0ahUKEwiNhJCjvZjzAhU6EVkFHX-hC4QQ4dUDCAk&uact=5";
+    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C5CHFA_enNZ948NZ948&oq=" + input + "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8"
 }
